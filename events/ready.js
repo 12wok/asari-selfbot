425366cfcc5a573e2adc8c0b3ba5c3d6ff04aa23
cex <43355709+outlaylol@users.noreply.github.com>
2020-02-08 08:38:41 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,7 @@ module.exports = (client) => {
     client.afk = new Discord.Collection()
     client.copycat = new Discord.Collection()
     client.snipe = new Discord.Collection()
+    client.editmode = new Discord.Collection()
     client.emotemode = new Discord.Collection()
     client.giveaway = new Discord.Collection()
     console.log(`Selfbot for ${client.user.tag} has been started.`)
