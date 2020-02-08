308d858a143128255d3f9e3394745bbffec7c1a9
cex <43355709+outlaylol@users.noreply.github.com>
2020-02-08 12:37:59 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,9 @@
-
-const r = require('request')
 const Discord = require('discord.js')
 module.exports = (client) => {
     client.afk = new Discord.Collection()
     client.copycat = new Discord.Collection()
     client.snipe = new Discord.Collection()
-
-    
+    client.emotemode = new Discord.Collection()
     console.log(`Selfbot for ${client.user.tag} has been started.`)
  
     
